d271805fe36e874c616067b3f61f1e02fe59e1c5
almarcesiblelu <jsilva@uni.pe>
2023-09-09 18:23:45 -0700

    add treesitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,4 +31,12 @@ return {
       },
     },
   },
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+  },
 }
