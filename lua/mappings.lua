9a22d824e2694f845098e35ce7f71462feaf0052
shrimpdevkro <jcsile444@gmail.com>
2023-12-27 12:27:55 -0500

    diagnostics

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -48,4 +48,9 @@ map.set("n", "<leader>Q", "<cmd>q!<CR>")
 
 -- togleterm
 map.set({ "n", "t" }, "<C-t>", "<cmd>ToggleTerm<CR>")
--- use control-x to exit terminal mode
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
